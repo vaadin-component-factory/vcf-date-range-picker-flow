a81b63eba35203ea946d017ca192862660839ac4
Paola De Bartolo <paola.debartolo@gmail.com>
2023-04-26 14:58:33 -0300

    build: update client side dependency to 4.8.13

--- vcf-date-range-picker/src/main/java/com/vaadin/componentfactory/EnhancedDateRangePicker.java
+++ vcf-date-range-picker/src/main/java/com/vaadin/componentfactory/EnhancedDateRangePicker.java
@@ -58,7 +58,7 @@ import elemental.json.JsonObject;
 @JavaScript("./date-fns-limited.min.js")
 @JavaScript("./enhancedDateRangePickerConnector.js")
 @Tag("vcf-date-range-picker")
-@NpmPackage(value = "@vaadin-component-factory/vcf-date-range-picker", version = "^4.8.11")
+@NpmPackage(value = "@vaadin-component-factory/vcf-date-range-picker", version = "4.8.13")
 @JsModule("@vaadin-component-factory/vcf-date-range-picker/vcf-date-range-picker.js")
 public class EnhancedDateRangePicker extends GeneratedVaadinDatePicker<EnhancedDateRangePicker, DateRange>
         implements HasSize, HasValidation, HasComponents {
